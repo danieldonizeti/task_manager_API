49579d3dfbea94ebaafc36e1c036e7dca8554edb
Daniel <danieldonizetidesouzajr@gmail.com>
2026-04-23 23:54:25 -0300

    fix: use dj_database_url

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
