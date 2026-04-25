d5cbd53469386cef0a80caff4dab6ab18a274b46
Daniel <danieldonizetidesouzajr@gmail.com>
2026-04-25 19:37:20 -0300

    rota de perfil para usuario

--- apps/users/views/user_view.py
+++ apps/users/views/user_view.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from apps.users.models import User
 from apps.users.serializers.user_serializer import UserSerializer, CreateUserSerializer
@@ -21,3 +23,8 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return CreateUserSerializer
         return UserSerializer
+    
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
