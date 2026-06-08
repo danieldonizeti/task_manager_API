c238b41d132a7c1b33373607f6316e8055e9b94a
Daniel <danieldonizetidesouzajr@gmail.com>
2026-06-08 16:15:59 -0300

    aumentar paginação

--- core/settings.py
+++ core/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':5,
+    'PAGE_SIZE':10,
 
     'EXCEPTION_HANDLER': 'common.exceptions.handler.custom_exception_handler',
 }
